6d1e688429f4a7942c32ef36e4a8a0e827f94bd7
sixmillions <liubw95@163.com>
2023-11-24 00:53:56 +0000

    simple dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="flex items-center justify-center">
+      <div>
+        <h1>Hello World !!</h1>
+
+        <button className="btn btn-primary">Button</button>
+
+        <div>
+          <ThemeSwitch />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
